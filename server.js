fe3ab412f27dc545d32ceab6f126c6db67dca301
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-21 12:12:18 +0530

    uuid install

--- server.js
+++ server.js
@@ -9,10 +9,14 @@
 // });
 
 const express = require("express");
-const app=express()
-app.get('/',(req,res)=>{
-    res.send('hello world')
-})
-app.listen(3000,(req,res)=>{
-    console.log("server is running on port 3000")
-})
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use("/api/users", require("./routes/API/user"));
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+

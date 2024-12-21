758f698917f11375ff5ac4207b6159255276aa97
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-21 11:53:19 +0530

    error

--- node.js
+++ node.js
@@ -1,9 +1,16 @@
-//node js server created
-var http = require('http');
+// //node js server created
+// var http = require('http');
 
-http.createServer(function (req, res) {
-    res.write('hello world');
-    res.end();
-}).listen(8000, function() {
-    console.log('Server running at http://localhost:8000/');
-});
+// http.createServer(function (req, res) {
+//     res.write('hello world');
+//     res.end();
+// }).listen(8000, function() {
+//     console.log('Server running at http://localhost:8000/');
+// });
+
+const express = require("express");
+const app=express()
+
+app.listen(4000,(req,res)=>{
+    console.log("server is running on port 4000")
+})

7aa2c64964ae03e023ff3b2812b88f3d4e8dd05a
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-21 19:38:50 +0530

    add Post for user.js

--- routes/API/users.js
+++ routes/API/users.js
@@ -3,18 +3,30 @@ const router = express.Router();
 const uuid = require("uuid");
 let users = require("../../Users");
 
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
   res.json(users);
-})
+});
 
-router.get("/:id",(req,res)=>{
-  const found=users.some((user) =>user.id ===parseInt(req.params.id));
-  if(found){
-    res.json(users.filter((user)=> user.id === parseInt(req.params.id)));
+router.get("/:id", (req, res) => {
+  const found = users.some((user) => user.id === parseInt(req.params.id));
+  if (found) {
+    res.json(users.filter((user) => user.id === parseInt(req.params.id)));
+  } else res.status(400).json({ message: "User not found" });
+});
+router.post("/", (req, res) => {
+  const newUser = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+  };
+  
+  if (!newUser.name || !newUser.email) { 
+    return res.sendStatus(400);
+  }
 
-}
-else(
-  res.status(400).json({message:"User not found"})
-)});
+  users.push(newUser);
+  res.json(users);
+});
 
-module.exports=router;
+
+module.exports = router;

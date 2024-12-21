a70f7198c7a60924fd514e786458895bb09ed9ce
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-21 10:40:56 +0530

    node js practice

---
+++ node.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.write('hello world');
+    res.end();
+}).listen(8000, function() {
+    console.log('Server running at http://localhost:8000/');
+});

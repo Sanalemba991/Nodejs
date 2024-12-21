5ff0cc0d7b3c63f6b567ca0ddfb4b11f0f18a96d
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-21 11:59:50 +0530

    json file created

---
+++ Users.js
@@ -0,0 +1,18 @@
+const users=[
+    {
+        id:1,
+        name:'Jhon',
+        email:'jhon@gmail.com',
+    
+    },{
+        id:2,
+        name:'Jane',
+        email:'jane@gmail.com',
+    }
+    ,{
+        id:3,
+        name:'Romeo',
+        email:'crish@gmail.com'
+    }
+]
+module.exports=users;

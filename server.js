fbb81b9f4e324cab0eba31f208c3ed9b2dfa1302
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-21 12:34:00 +0530

    intry for json format

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Route to handle API users
 app.use("/api/users", require("./routes/API/user"));
-
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");

46901ed158bd58e2730998e17ef242a4491ef34d
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-22 17:28:35 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port number 3000');
+});

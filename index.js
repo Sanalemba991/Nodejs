04ad9b7a75d33d4ef2b3b11301361fe1e51fc13a
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-22 11:40:06 +0530

    post and get data by id is added

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+//parse JSON using express
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+let movies = [
+  {
+    id: 1,
+    title: "The Shawshank Redemption",
+    year: 1994,
+    genre: "Drama",
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    year: 1972,
+    genre: "Crime",
+  },
+];
+
+app.get("/movie", (req, res) => {
+  res.json(movies);
+});
+app.post("/movie", (req, res) => {
+  const movie = req.body;
+  console.log(movie);
+  movies.push(movie);
+  res.send("Movie is added to the List of Movie");
+});
+
+app.get("/movie/:id",(req,res)=>{
+    const id=req.params.id
+    for (let movie of movies){
+        if(movie.id ==id){
+            res.json(movie)
+            return
+        }
+    }
+    res.status(404).json({message:"Movie not found"})
+})
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
